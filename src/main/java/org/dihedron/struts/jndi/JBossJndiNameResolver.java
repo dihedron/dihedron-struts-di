608aae8b00b9bc199979ef2979a6724b9090193c
Andrea Funto' <dihedron.dev@gmail.com>
2015-08-21 15:41:03 +0200

    Initial commit

--- src/main/java/org/dihedron/struts/jndi/JBossJndiNameResolver.java
+++ src/main/java/org/dihedron/struts/jndi/JBossJndiNameResolver.java
@@ -1,20 +1,5 @@
-/**
- * Copyright (c) 2013, Andrea Funto'. All rights reserved.
- * 
- * This file is part of the Dihedron Struts2 Dependency Injection Plugin ("Struts-DI").
- *
- * "Struts-DI" is free software: you can redistribute it and/or modify it under 
- * the terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation, either version 3 of the License, or (at your option) 
- * any later version.
- *
- * "Struts-DI" is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License 
- * along with "Struts-DI". If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright (c) 2012-2015, Andrea Funto'. All rights reserved. See LICENSE for details.
  */
 package org.dihedron.struts.jndi;
 
